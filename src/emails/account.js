a0c170d1bf7b49e4b1c25257aa8f0b19a0d0ac64
FarZad <farzad78es@gmail.com>
2022-03-15 20:02:53 +0330

    Init commit

---
+++ src/emails/account.js
@@ -0,0 +1,52 @@
+const nodemailer=require('nodemailer')
+
+const sendWelcomeEmail=async(email, name)=>{
+    let welcomeAccountForTest=await nodemailer.createTestAccount()
+
+    let transporter=nodemailer.createTransport({
+        host:'smtp.ethereal.email',
+        port:587,
+        secure:false,
+        auth:{
+            user:welcomeAccountForTest.user,
+            pass:welcomeAccountForTest.pass
+        }
+    })
+
+    let info=await transporter.sendMail({
+        from:'farzad78es@gmail.com',
+        to:email,
+        subject:'Thanks for joining in',
+        text:`Welcome to task manager app, ${name}. Let me know how it's going with the app`
+    })
+    console.log(info)
+    console.log(nodemailer.getTestMessageUrl(info))
+}
+
+const sendCancelationEmail=async(email, name)=>{
+    let cancelationAccountForTest=await nodemailer.createTestAccount()
+
+    let transporter=nodemailer.createTransport({
+        host:'smtp.ethereal.email',
+        port:587,
+        secure:false,
+        auth:{
+            user:cancelationAccountForTest.user,
+            pass:cancelationAccountForTest.pass,
+        }
+    })
+
+    let info=await transporter.sendMail({
+        from:'farzad78es@gmail.com',
+        to:email,
+        subject:'Sorry to see you go!',
+        text:`Thanks for using our app, ${name}. Please let us know your feedback to make our app better`
+    })
+    console.log(info)
+    console.log(nodemailer.getTestMessageUrl(info))
+}
+
+module.exports={
+    sendWelcomeEmail,
+    sendCancelationEmail
+}
---
+++ src/routers/user.js
@@ -0,0 +1,155 @@
+const express=require('express')
+const multer=require('multer')
+const sharp=require('sharp')
+const User=require('../models/user.js')
+const mails=require('../emails/account.js')
+const auth=require('../middleware/authenticaion.js')
+
+
+const router=new express.Router()
+
+//Creating users
+router.post('/users',async (req,res)=>{
+    const user=new User(req.body)
+    
+    try{
+        const token=await user.generateAuthenticationToken()
+        await user.save()
+        mails.sendWelcomeEmail(user.email, user.name)
+        res.status(201).send({user, token})
+    } catch(error){
+        res.status(400).send(error)
+    }
+})
+
+//Reading my profile
+router.get('/users/me', auth, async (req,res)=>{
+    try{
+        res.send(req.user)
+    } catch(error){
+        res.status(500).send()
+    }
+})
+
+//Updating my profile
+router.patch('/users/me', auth, async (req,res)=>{
+    const allowedUpdates=['name','email','age','password']
+    const updates=Object.keys(req.body)
+    const isYourUpdateValid=updates.every((update)=>allowedUpdates.includes(update))
+    
+    if(!isYourUpdateValid){
+        return res.status(400).send({error:'Invalid update'})
+    }
+    
+    try{
+        updates.forEach((update)=>req.user[update]=req.body[update])
+        await req.user.save()
+    
+        res.status(200).send(req.user)
+    } catch(error){
+        res.status(400).send(error)
+    }
+})
+
+//Deleting my profile
+router.delete('/users/me', auth, async(req,res)=>{
+    try{
+        await req.user.remove()
+        mails.sendCancelationEmail(req.user.email, req.user.name)
+        res.send(req.user)
+    } catch(error){
+        res.status(400).send()
+    }
+})
+
+//Uploading user profile picture
+const uploadProfilePic=multer({
+    limits:{
+        fileSize:1000000
+    },
+    fileFilter(req, file, cb){
+        if(!file.originalname.match(/\.(jpg|jpeg|png)$/)){
+            return cb(new Error('Please upload a jpg or jpeg or png file'))
+        }
+
+        cb(undefined, true)
+    }
+})
+
+router.post('/users/me/avatar', auth, uploadProfilePic.single('avatar'), async(req, res)=>{
+    const buffer=await sharp(req.file.buffer).resize({ width:300, height:300 }).png().toBuffer()
+    req.user.avatar=buffer
+    await req.user.save()
+
+    res.send()
+}, (error, req, res, next)=>{
+    res.status(400).send({Error:error.message})
+})
+
+//Deleting user profile picture
+router.delete('/users/me/avatar', auth, async(req, res)=>{
+    try{
+        req.user.avatar=undefined
+        await req.user.save()
+        
+        res.status(200).send()
+    } catch(error){
+        res.status(500).send()
+    }
+})
+
+//Getting user profile pic from server
+router.get('/users/:id/avatar', async(req, res)=>{
+    try{
+        const user=await User.findById(req.params.id)
+
+        if((!user) || (!user.avatar)){
+            throw new Error()
+        }
+
+        res.set('Content-Type', 'image/jpg')
+        res.send(user.avatar)
+    } catch(error){
+        res.status(404).send()
+    }
+})
+
+//Login as a user
+router.post('/users/login', async(req,res)=>{
+    try{
+        const user=await User.findByCredentials(req.body.email, req.body.password)
+
+        const token=await user.generateAuthenticationToken()
+        res.send({user, token})
+    } catch(error){
+        res.status(400).send(error)
+    }
+})
+
+//Logout handler
+router.post('/users/logout', auth, async(req, res)=>{
+    try{
+        req.user.tokens=req.user.tokens.filter((token)=>{
+            return req.token !== req.token
+        })
+
+        await req.user.save()
+        res.send()
+    } catch(error){
+        res.status(500).send(error)
+    }
+})
+
+//Logout from all sessions
+router.post('/users/logoutall', auth, async(req, res)=>{
+    try{
+        req.user.tokens.splice(0, req.user.tokens.length)
+        
+        await req.user.save()
+        res.send()
+    } catch(error){
+        res.status(500).send()
+    }
+})
+
+module.exports=router
